d6fc606d3fe8a548c2712b07d7b3ae13a4c6f756
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-07-23 11:39:30 +0530

    credential added

--- src/app/dashboard/dashboard.tsx
+++ src/app/dashboard/dashboard.tsx
@@ -15,12 +15,14 @@ export default function Dashboard() {
 
   useEffect(() => {
     init();
-  },[]);
+  }, []);
 
   async function init() {
     try {
       dispatch(loader.loader({ loader: true }));
-      const response = await axios.get("./api/dashboard");
+      const response = await axios.get("./api/dashboard", {
+        withCredentials: true,
+      });
       toast.success(response.data.message);
       setUser(response.data.data);
     } catch (er) {
@@ -44,7 +46,7 @@ export default function Dashboard() {
       {user && (
         <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
           <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight text-center">
-           {user.userName}
+            {user.userName}
           </h3>
           <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm text-center">
             {user.email}
