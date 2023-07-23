afd32d4ac51b36be9c35fee63a1793114007d1cb
Aswinthgt <109474329+Aswinthgt@users.noreply.github.com>
2023-07-23 12:21:28 +0530

    fixed

--- src/helper/jwt.ts
+++ src/helper/jwt.ts
@@ -1,19 +1,20 @@
 import jwt from "jsonwebtoken";
 import { NextRequest } from "next/server";
 
-export async function verifyToken(req: NextRequest) {
-    try {
-        const reqBody = req.cookies.get("token");
-        const token = reqBody?.value;
-        const tokenData = jwt.verify(token!, process.env.JWT_SECRET_KEY!);
-        if (tokenData) {
-            return tokenData;
-        } else {
-            throw new Error("token Verication Failed")
-        }
-    } catch (er) {
+export function verifyToken(req :NextRequest) {
+   try{
+    const reqBody = req.cookies.get("token");
+    const token = reqBody?.value;
+
+    const tokenData = jwt.verify(token!,process.env.JWT_SECRET_KEY!);
+    if(tokenData){
+        return tokenData;
+    }else {
         throw new Error("token Verication Failed")
     }
-
+   }catch(er){
+    throw new Error("token Verication Failed")
+   }
+   
 
 }
